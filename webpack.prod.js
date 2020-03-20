38a09c1d9455cfeffb658aa8bddd299e896f41ee
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-03-20 12:22:36 -0400

    feat: add workbox

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -45,6 +47,30 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({ filename: '[name].css' })
+        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new WorkboxPlugin.GenerateSW({
+            // Do not precache images
+            exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+
+            // Define runtime caching rules.
+            runtimeCaching: [{
+                // Match any request that ends with .png, .jpg, .jpeg or .svg.
+                urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+                // Apply a cache-first strategy.
+                handler: 'CacheFirst',
+
+                options: {
+                    // Use a custom cache name.
+                    cacheName: 'images',
+
+                    // Only cache 10 images.
+                    expiration: {
+                        maxEntries: 10,
+                    },
+                },
+            }],
+        })
+
     ]
 }
