b65fb2d5fc7a43e52fd64c67b69d2511c7cb5c46
Jeff Graham <jgraham@dcmetrowebdevelopment.com>
2020-04-18 18:28:55 -0400

    chore: update dependencies

--- src/server/index.js
+++ src/server/index.js
@@ -47,8 +47,8 @@ app.post('/testing', (req, res) => {
 
 })
 
-
+const port = process.env.PORT || 8081
 // designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
 })
